8fc8c3c07501cba067fc52d78e2d7ca30dce35f3
Allan <ismailov14091997@gmail.com>
2021-03-13 13:57:16 +0500

    new

---
+++ #sours/js/javascript.js
@@ -0,0 +1,28 @@
+
+//урок номер 7
+//!это 3 цыкла
+//!1)
+// let asd = 1;
+// while (asd < 6) {
+//    console.log(asd);
+//    asd++;
+// }
+//!2)
+// do {
+//    console.log(asd);
+//    asd++;
+// } while (asd < 6);
+//!3)
+// for (asd; asd < 10; asd++) {
+//    console.log(asd);
+//    if (asd == 5) break;
+// }
+//!это перевод с for на while
+// for (let num = 0; num < 3; num++) {
+//    console.log(`Число: ${num}`);
+// }
+// let num = 0;
+// while (num < 3) {
+//    console.log(`Число: ${num}`);
+//    num++;
+// }
